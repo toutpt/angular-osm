bcd45a7eb9fa3a30d212fff6993070cfe7941216
JeanMichel FRANCOIS <toutpt@gmail.com>
2016-05-10 17:33:36 +0200

    fix(oauth): add provider to configure it

--- src/oauth/oauth.js
+++ src/oauth/oauth.js
@@ -1,6 +1,12 @@
 import osmAuthService from './oauth.service';
 
 var osmOAuthModule = angular.module('osm.oauth', [])
-.factory('osmAuthService', osmAuthService);
+.factory('osmAuthService', osmAuthService)
+.provider('osmAuthService', function osmAuthServiceProvider () {
+    this.options = {};
+    this.$get = function osmAuthServiceFactory() {
+        return new osmAuthService(this.options);
+    };
+});
 
 export default osmOAuthModule;
