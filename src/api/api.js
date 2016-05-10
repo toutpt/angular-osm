af81fe08088ce091f472c116bc05b5a44299cdec
JeanMichel FRANCOIS <toutpt@gmail.com>
2016-05-10 17:29:32 +0200

    chore(refactor): move parts of api into osmUtilsService + add osmAPI example

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,15 @@
+import base64 from 'angular-base64';
 import osmAPI from './api.service';
 import osmSettingsModule from '../settings/settings';
+import osmUtilsModule from '../utils/utils';
 
-var osmAPIModule = angular.module('osm.api', [osmSettingsModule.name])
-.factory('osmAPI', osmAPI);
+//The base64 module is only available as IIFE
+
+var osmAPIModule = angular.module('osm.api', [
+    osmSettingsModule.name,
+    osmUtilsModule.name,
+    'base64'
+])
+.service('osmAPI', osmAPI);
 
 export default osmAPIModule;
