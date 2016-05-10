b3f78d56305097f93c857065b03592396bc78394
JeanMichel FRANCOIS <toutpt@gmail.com>
2016-05-10 18:04:21 +0200

    fix(oauth): make it work + add example

--- src/oauth/oauth.js
+++ src/oauth/oauth.js
@@ -4,9 +4,11 @@ var osmOAuthModule = angular.module('osm.oauth', [])
 .factory('osmAuthService', osmAuthService)
 .provider('osmAuthService', function osmAuthServiceProvider () {
     this.options = {};
-    this.$get = function osmAuthServiceFactory() {
-        return new osmAuthService(this.options);
+    
+    this.$get = function osmAuthServiceFactory($q) {
+        return new osmAuthService($q, this.options);
     };
+    this.$get.$inject = ['$q'];
 });
 
 export default osmOAuthModule;
--- src/oauth/oauth.service.js
+++ src/oauth/oauth.service.js
@@ -6,17 +6,43 @@ import osmAuth from 'osm-auth';
 * @requires angular-osm.osmSettingsService
 * @description handle osm oauth
 */
-function osmAuthService(options) {
+function osmAuthService($q, options) {
     if (options) {
         if (options.oauth_secret && options.oauth_consumer_key) {
-            osmAuth(options);
+            this.auth = osmAuth(options);
         }
     }
-    this.logout = osmAuth.logout;
-    this.authenticated = osmAuth.authenticated;
-    this.authenticate = osmAuth.authenticate;
-    this.xhr = osmAuth.xhr;
-    this.options = osmAuth.options;
+    this.logout = function () {
+        return this.auth.logout();
+    };
+    this.authenticated = function () {
+        return this.auth.authenticated();
+    };
+    this.authenticate = function () {
+        var deferred = $q.defer();
+        this.auth.authenticate(function () {
+            deferred.resolve(true);
+        });
+        return deferred.promise;
+    };
+    this.xhr = function (options) {
+        var deferred = $q.defer();
+        this.auth.xhr(options, function (err, data) {
+            if (err) {
+                deferred.reject(err);
+            } else {
+                deferred.resolve(data);
+            }
+        });
+        return deferred.promise;
+    };
+    this.options = function (options) {
+        if (this.auth) {
+            this.auth.options(options);
+        } else {
+            this.auth = osmAuth(options);
+        }
+    };
 }
 
 export default osmAuthService;
