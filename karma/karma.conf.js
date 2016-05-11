666d422ba970f999e18ff3b9e63c4db5f33b373b
JeanMichel FRANCOIS <toutpt@gmail.com>
2016-05-11 14:39:56 +0200

    fix(karma): test now running

--- karma/karma.conf.js
+++ karma/karma.conf.js
@@ -2,21 +2,18 @@ module.exports = function(config){
     config.set({
         basePath : '',
         files : [
-            {pattern: '../dist/dependencies.js', watched: true, included: true, nocache: true},
+            {pattern: '../node_modules/angular/angular.min.js', watched: true, included: true, nocache: true},
+            {pattern: '../node_modules/ngstorage/ngStorage.min.js', watched: true, included: true, nocache: true},
+            {pattern: '../node_modules/angular-base64/angular-base64.min.js', watched: true, included: true, nocache: true},
             {pattern: '../node_modules/osmtogeojson/osmtogeojson.js', watched: true, included: true, nocache: true},
             {pattern: '../node_modules/angular-mocks/angular-mocks.js', watched: false, included: true, served: true},
             {pattern: '../node_modules/ng-describe/dist/ng-describe.js', watched: false, included: true, served: true},
-            {pattern: '../dist/osm.js', watched: true, included: true, nocache: true},
+            {pattern: '../dist/osm-full.js', watched: true, included: true, nocache: true},
             {pattern: '../src/**/*.spec.js', watched: true, included: true, nocache: true}
         ],
         exclude : [],
         autoWatch : true,
         frameworks: ['jasmine'],
-        plugins : [
-            'karma-jasmine',
-            'karma-chrome-launcher',
-            'karma-firefox-launcher'
-        ],
         reporters: ['dots'],
         logLevel: 'LOG_DEBUG'
     });
