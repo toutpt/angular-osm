970de177f84808b9947ace889a4ddf2d326ca6a3
JeanMichel FRANCOIS <toutpt@gmail.com>
2016-05-11 14:45:17 +0200

    fix(test): refactor karma config

---
+++ karma/conf.js
@@ -0,0 +1,18 @@
+module.exports = {
+    basePath : '',
+    files : [
+        {pattern: '../node_modules/angular/angular.min.js', watched: true, included: true, nocache: true},
+        {pattern: '../node_modules/ngstorage/ngStorage.min.js', watched: true, included: true, nocache: true},
+        {pattern: '../node_modules/angular-base64/angular-base64.min.js', watched: true, included: true, nocache: true},
+        {pattern: '../node_modules/osmtogeojson/osmtogeojson.js', watched: true, included: true, nocache: true},
+        {pattern: '../node_modules/angular-mocks/angular-mocks.js', watched: false, included: true, served: true},
+        {pattern: '../node_modules/ng-describe/dist/ng-describe.js', watched: false, included: true, served: true},
+        {pattern: '../dist/osm-full.js', watched: true, included: true, nocache: true},
+        {pattern: '../src/**/*.spec.js', watched: true, included: true, nocache: true}
+    ],
+    exclude : [],
+    autoWatch : true,
+    frameworks: ['jasmine'],
+    reporters: ['dots'],
+    logLevel: 'LOG_DEBUG'
+};
--- karma/karma.conf.js
+++ karma/karma.conf.js
@@ -1,20 +1,5 @@
+var conf = require('./conf');
+
 module.exports = function(config){
-    config.set({
-        basePath : '',
-        files : [
-            {pattern: '../node_modules/angular/angular.min.js', watched: true, included: true, nocache: true},
-            {pattern: '../node_modules/ngstorage/ngStorage.min.js', watched: true, included: true, nocache: true},
-            {pattern: '../node_modules/angular-base64/angular-base64.min.js', watched: true, included: true, nocache: true},
-            {pattern: '../node_modules/osmtogeojson/osmtogeojson.js', watched: true, included: true, nocache: true},
-            {pattern: '../node_modules/angular-mocks/angular-mocks.js', watched: false, included: true, served: true},
-            {pattern: '../node_modules/ng-describe/dist/ng-describe.js', watched: false, included: true, served: true},
-            {pattern: '../dist/osm-full.js', watched: true, included: true, nocache: true},
-            {pattern: '../src/**/*.spec.js', watched: true, included: true, nocache: true}
-        ],
-        exclude : [],
-        autoWatch : true,
-        frameworks: ['jasmine'],
-        reporters: ['dots'],
-        logLevel: 'LOG_DEBUG'
-    });
+    config.set(conf);
 };
