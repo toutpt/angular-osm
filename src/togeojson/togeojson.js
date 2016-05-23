dc18e43953c0d37c3cfe54041f93cb8dd7790067
JeanMichel FRANCOIS <toutpt@gmail.com>
2016-05-23 15:18:35 +0200

    fix(togeojson): remove factory call, service already define throw the provider

--- src/togeojson/togeojson.js
+++ src/togeojson/togeojson.js
@@ -1,8 +1,6 @@
 import provider from './togeojson.provider';
-import factory from './togeojson.factory';
 
 var osmtogeojsonModule = angular.module('osm.togeojson', [])
-.factory('osmtogeojson', factory)
 .provider('osmtogeojson', provider);
 
 export default osmtogeojsonModule;
