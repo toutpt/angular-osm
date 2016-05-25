169df405a750b12547465a4dcf5afb032d8e0f97
JeanMichel FRANCOIS <toutpt@gmail.com>
2016-05-25 15:42:03 +0200

    chore(travis): push lconv to coveralls

--- karma/karma.travis.conf.js
+++ karma/karma.travis.conf.js
@@ -2,7 +2,7 @@ var conf = require('./conf');
 
 module.exports = function(config){
     conf.browsers = ['PhantomJS'];
-    conf.reporters.push('coverage', 'coveralls');
+    conf.reporters.push('coverage');
     conf.preprocessors['../dist/osm-full.js'] = ['coverage'];
     config.singleRun = true;
     conf.coverageReporter.type = 'lcov';
