f4eff38944b2d09d7d4942ae08b9b32c741fbba3
Thomas F <tom.fougere@gmail.com>
2016-11-18 16:21:14 +0100

    fix(overpass): use // for the api URL (#4)

--- src/overpass/overpass.js
+++ src/overpass/overpass.js
@@ -6,7 +6,7 @@ import osmOverpassAPI from './overpass.service';
 var osmOverpassModule = angular.module('osm.overpass', [])
 .provider('osmOverpassAPI', function osmOverpassAPIProvider () {
     this.options = {
-        url: 'http://overpass-api.de/api/interpreter'
+        url: '//overpass-api.de/api/interpreter'
     };
     this.$get = function osmOverpassAPIFactory($http, $q) {
         return new osmOverpassAPI($http, $q, this.options);
